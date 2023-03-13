7ef5451b68f510874b85fd5c70008840a4e69f7c
tgorschin <127777090+tgorschin@users.noreply.github.com>
2023-03-13 15:54:22 -0300

    EJ1

---
+++ CHIRAMBERRO CLASE 1/src/ej1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ej1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n;
+		
+		System.out.println("Ingresa el numero que quieras");
+		n = sc.nextInt();
+
+		System.out.println("El numero es "+ n);
+	}
+
+}
