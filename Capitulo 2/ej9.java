780e2e4630b13cd974477815b1467f40846eee56
tgorschin <127777090+tgorschin@users.noreply.github.com>
2023-04-10 17:31:09 -0300

    Create ej9.java

---
+++ Capitulo 2/ej9.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ej1 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int a;
+		System.out.println("Ingrese un numero entre 0 y 99.999");
+		System.out.println("A:");
+		a = sc.nextInt();
+		
+		 System.out.println("El número " + a + " tiene " + Integer.toString(a).length() + " cifras");
+	}}
+
