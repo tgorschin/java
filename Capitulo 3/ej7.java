4eef16d9e3b69a202b7b60b6eeba2fedb821e4e0
tgorschin <127777090+tgorschin@users.noreply.github.com>
2023-05-22 17:23:51 -0300

    Create ej7.java

---
+++ Capitulo 3/ej7.java
@@ -0,0 +1,13 @@
+package tp3;
+import java.util.*;
+public class ej7 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int cont=1;
+		
+		for (int i=1;i<=10;i++) {
+			System.out.println(cont);
+			cont=cont+2;
+		}
+	}
+}
