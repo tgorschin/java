ded1a9e5b6edb63174f5aa2aed4de945df4b0528
tgorschin <127777090+tgorschin@users.noreply.github.com>
2023-05-22 17:25:28 -0300

    Create ej11.java

---
+++ Capitulo 3/ej11.java
@@ -0,0 +1,29 @@
+package Default;
+
+public class A3_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1=1, num2=1;
+		while (num1<=10) {
+			num2=1;
+			while (num2 <= 10){
+				
+				System.out.print(num1*num2+" ");
+				num2++;
+			}
+			num1++;
+			System.out.println(" ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
