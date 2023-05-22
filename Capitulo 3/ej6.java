fd5ae03eda4b3addbc739e57de8fb4cf336459a5
tgorschin <127777090+tgorschin@users.noreply.github.com>
2023-05-22 17:23:28 -0300

    Create ej6.java

---
+++ Capitulo 3/ej6.java
@@ -0,0 +1,12 @@
+package tp3;
+import java.util.*;
+public class ej6 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("multiplo");
+		int multiplo=0;
+	
+		for (int i=14;i>0;i--) {
+			multiplo=i*7;
+			System.out.println(multiplo);			
+		}}}
